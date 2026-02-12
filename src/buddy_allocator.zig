8cc8b61f6a462d943cc1ebe1d5443699f76668a1
Wilmer Fredriksson Handler <wilmerfhandler@gmail.com>
2026-02-12 14:02:27 +0100

    feat: represent memory as a tree

--- src/buddy_allocator.zig
+++ src/buddy_allocator.zig
@@ -1,15 +1,59 @@
 const std = @import("std");
 
+pub const MemoryTree = struct {
+    pub const State = enum { free, split, used };
+    buffer: []u8,
+    state: State = .free,
+    leftChild: ?MemoryTree,
+    rightChild: ?MemoryTree,
+
+    fn init(buffer: []u8, min_block_size: u8) ?MemoryTree {
+        if (buffer.len < min_block_size) {
+            return null;
+        }
+
+        if (buffer.len < min_block_size * 2) {
+            return MemoryTree{
+                .buffer = buffer,
+                .state = .free,
+                .leftChild = null,
+                .rightChild = null,
+            };
+        }
+
+        const mid = buffer.len / 2;
+        const left_half = buffer[0..mid];
+        const right_half = buffer[mid..];
+
+        return MemoryTree{
+            .buffer = buffer,
+            .state = .free,
+            .leftChild = init(left_half, min_block_size),
+            .rightchild = init(right_half, min_block_size),
+        };
+    }
+};
+
 pub const BuddyAllocator = struct {
     buffer: []u8,
+    min_block_size: usize,
+    root: MemoryTree,
 
-    pub fn init(buffer: []u8) BuddyAllocator {
-        return BuddyAllocator{ .buffer = buffer };
+    fn alloc(ctx: *anyopaque, len: usize, alignment: std.mem.Alignment, ret_addr: usize) ?[*]u8 {
+        _ = ctx;
+        _ = len;
+        _ = alignment;
+        _ = ret_addr;
+
+        return null;
     }
 
-    fn alloc(ctx: *anyopaque, len: usize, alignment: std.mem.Alignment, ret_addr: usize) ?[*]u8 {}
-
-    fn free(ctx: *anyopaque, memory: []u8, alignment: std.mem.Alignment, ret_addr: usize) void {}
+    fn free(ctx: *anyopaque, memory: []u8, alignment: std.mem.Alignment, ret_addr: usize) void {
+        _ = ctx;
+        _ = memory;
+        _ = alignment;
+        _ = ret_addr;
+    }
 
     fn resize(
         ctx: *anyopaque,
@@ -17,7 +61,14 @@ pub const BuddyAllocator = struct {
         alignment: std.mem.Alignment,
         new_len: usize,
         ret_addr: usize,
-    ) bool {}
+    ) bool {
+        _ = ctx;
+        _ = memory;
+        _ = alignment;
+        _ = new_len;
+        _ = ret_addr;
+        return false;
+    }
 
     fn remap(
         ctx: *anyopaque,
@@ -25,7 +76,14 @@ pub const BuddyAllocator = struct {
         alignment: std.mem.Alignment,
         new_len: usize,
         ret_addr: usize,
-    ) ?[*]u8 {}
+    ) ?[*]u8 {
+        _ = ctx;
+        _ = memory;
+        _ = alignment;
+        _ = new_len;
+        _ = ret_addr;
+        return null;
+    }
 
     const vtable = std.mem.Allocator.VTable{
         .alloc = alloc,
